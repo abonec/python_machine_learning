86dbd3b3a86c80882cdf744a6f15c958c845d0b1
abonec <abonec@gmail.com>
2016-05-13 12:11:20 +0300

    plot the iris dataset

--- chapter2/perception.py
+++ chapter2/perception.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 class Perception(object):
     def __init__(self, eta=0.01, n_iter=10):
         self.eta = eta
@@ -14,7 +17,7 @@ class Perception(object):
                 update = self.eta * (target - self.predict(xi))
                 self.w_[1:] += update * xi
                 self.w_[0] += update
-                erors += int(update != 0.0)
+                errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
 
@@ -24,7 +27,19 @@ class Perception(object):
     def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, -1)
 
-import pandas as pd
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-print df.tail()
+y = df.iloc[0:100, 4].values
+
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0,2]].values
+
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+
+plt.xlabel('petal length')
+plt.ylabel('sepal length')
+plt.legend(loc='upper left')
+
+plt.show()
+
