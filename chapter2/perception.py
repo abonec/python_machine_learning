a61cdc1d02359094e787170ffdd7a98c334f5b73
abonec <abonec@gmail.com>
2016-05-13 12:14:59 +0300

    fit the model and plot the errors versus number of epochs

--- chapter2/perception.py
+++ chapter2/perception.py
@@ -43,3 +43,12 @@ plt.legend(loc='upper left')
 
 plt.show()
 
+
+ppn = Perception(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+plt.show()
+
