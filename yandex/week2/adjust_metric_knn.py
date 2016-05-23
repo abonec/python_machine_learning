cde121e7dfd8576e3855bf349f7138d62c43cc09
abonec <abonec@gmail.com>
2016-05-23 14:11:58 +0300

    complete assignment for adjusting metrics for knn

---
+++ yandex/week2/adjust_metric_knn.py
@@ -0,0 +1,28 @@
+import pandas
+import sklearn.datasets
+import numpy as np
+from sklearn.cross_validation import KFold
+from sklearn.cross_validation import cross_val_score
+from sklearn.preprocessing import scale
+from sklearn.neighbors import KNeighborsRegressor
+import output_coursera as coursera
+
+data = sklearn.datasets.load_boston()
+X = pandas.DataFrame(data.data)
+y = pandas.DataFrame(data.target)
+X_scaled = pandas.DataFrame(scale(X))
+
+parameters = np.linspace(start=1, stop=10, num=200)
+best_p = 0
+best_score = -100
+for p in parameters:
+    clf = KNeighborsRegressor(n_neighbors=5, weights='distance', p=p)
+    k_folder = KFold(X_scaled.shape[0], n_folds=5, random_state=42, shuffle=True)
+    scores = cross_val_score(clf, X=X_scaled, y=y, scoring='mean_squared_error', cv=k_folder)
+    mean = np.mean(scores)
+    print(scores)
+    if mean > best_score:
+        best_p = p
+        best_score = mean
+
+coursera.output('adjust_metric.txt', "{:.0f}".format(best_p))
