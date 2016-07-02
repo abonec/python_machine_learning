c4b4d6d68b34f25916d5368ea28f03e307a0e434
Alexander Baronec <abonec@gmail.com>
2016-07-02 17:17:04 +0300

    add bag of words

--- yandex/week7/final_boosting.py
+++ yandex/week7/final_boosting.py
@@ -7,7 +7,7 @@ from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
-features_set = pandas.read_csv('features.csv')
+features_set = pandas.read_csv('features.csv',index_col='match_id')
 X = features_set.drop(['duration', 'radiant_win', 'tower_status_radiant', 'tower_status_dire', 'barracks_status_radiant', 'barracks_status_dire'], axis=1)
 y = features_set[['radiant_win']].values.ravel()
 
@@ -27,7 +27,7 @@ X = X.fillna(0)
 size = 0
 score = 0
 for forest_size in [10, 20, 30, 50, 150, 300]:
-    # break
+    break
     start_time = datetime.datetime.now()
     clf = GradientBoostingClassifier(n_estimators=forest_size)
     k_folder = KFold(X.shape[0], n_folds=5, shuffle=True)
@@ -42,10 +42,9 @@ print("best score was for {} forest size: {}".format(size, score))
 
 # ===================LogisticRegression=================
 features = X
-
-X = StandardScaler().fit_transform(features)
-
 def train_logistic(features, target, label):
+    # return
+    features = StandardScaler().fit_transform(features)
     clf = LogisticRegression(penalty='l2')
     k_folder = KFold(features.shape[0], n_folds=5, shuffle=True)
     start_time = datetime.datetime.now()
@@ -60,8 +59,30 @@ for i in range(1, 5+1):
     category_features.append("r{}_hero".format(i))
     category_features.append("d{}_hero".format(i))
 
-X = features.drop(category_features, axis=1)
-X = StandardScaler().fit_transform(X)
+X = X.drop(category_features, axis=1)
 
 train_logistic(X, y, 'Drop categorial features')
+
+
+heros_features = category_features[1:len(category_features)]
+hero_ids = []
+for feature_name in heros_features:
+    for id in features[feature_name].unique().tolist():
+        hero_ids.append(id)
+
+hero_ids = np.unique(hero_ids)
+unique_names = len(hero_ids)
+print("there is {} unique heroes".format(unique_names))
+
+X_pick = np.zeros((features.shape[0], 112))
+
+for i, match_id in enumerate(features.index):
+    for p in range(5):
+        X_pick[i, features.ix[match_id, 'r{}_hero'.format(p+1)]-1] = 1
+        X_pick[i, features.ix[match_id, 'd{}_hero'.format(p+1)]-1] = -1
+
+
+X = np.concatenate([X, X_pick], axis=1)
+
+train_logistic(X, y, 'With Bag of Words')
 
